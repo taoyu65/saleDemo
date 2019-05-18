ffa0f31575d790a19520e516a33fbc75ce35ecf6
Tao <taoyu65@gmail.com>
2019-05-18 11:47:12 -0700

    default

---
+++ saleDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace saleDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
